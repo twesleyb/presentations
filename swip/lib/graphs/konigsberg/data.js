ce5e1cb2716acce71b7a599bf43f894ad2364e20
twesleyb <twesleyb10@gmail.com>
2021-06-29 15:44:28 -0400

    rewind and fix-up

---
+++ swip/lib/graphs/konigsberg/data.js
@@ -0,0 +1,152 @@
+let data = {
+		"format_version" : "1.0",
+		"generated_by" : "cytoscape-3.8.2",
+		"target_cytoscapejs_version" : "~2.1",
+		"data" : {
+				"shared_name" : "konigsberg.csv",
+				"name" : "konigsberg.csv",
+				"SUID" : 61,
+				"__Annotations" : [ ],
+				"selected" : true
+		},
+		"elements" : {
+				"nodes" : [ {
+						"data" : {
+								"id" : "78",
+								"shared_name" : "Lomse",
+								"name" : "Lomse",
+								"SUID" : 78,
+								"selected" : false
+						},
+						"position" : {
+								"x" : 48.01087173553449,
+								"y" : -6.816946144648217
+						},
+						"selected" : false
+				}, {
+						"data" : {
+								"id" : "75",
+								"shared_name" : "South",
+								"name" : "South",
+								"SUID" : 75,
+								"selected" : false
+						},
+						"position" : {
+								"x" : -25.42422045988492,
+								"y" : 62.277062047273034
+						},
+						"selected" : false
+				}, {
+						"data" : {
+								"id" : "72",
+								"shared_name" : "Kniephof",
+								"name" : "Kniephof",
+								"SUID" : 72,
+								"selected" : false
+						},
+						"position" : {
+								"x" : -52.3805371411146,
+								"y" : 0.4207771275293406
+						},
+						"selected" : false
+				}, {
+						"data" : {
+								"id" : "71",
+								"shared_name" : "North",
+								"name" : "North",
+								"SUID" : 71,
+								"selected" : false
+						},
+						"position" : {
+								"x" : -42.37366480638078,
+								"y" : -60.06918838164519
+						},
+						"selected" : false
+				} ],
+				"edges" : [ {
+						"data" : {
+								"id" : "79",
+								"source" : "78",
+								"target" : "72",
+								"shared_name" : "Lomse (interacts with) Kniephof",
+								"shared_interaction" : "interacts with",
+								"name" : "Lomse (interacts with) Kniephof",
+								"interaction" : "interacts with",
+								"SUID" : 79,
+								"Weight" : 1,
+								"selected" : false
+						},
+						"selected" : false
+				}, {
+						"data" : {
+								"id" : "80",
+								"source" : "75",
+								"target" : "78",
+								"shared_name" : "South (interacts with) Lomse",
+								"shared_interaction" : "interacts with",
+								"name" : "South (interacts with) Lomse",
+								"interaction" : "interacts with",
+								"SUID" : 80,
+								"Weight" : 1,
+								"selected" : false
+						},
+						"selected" : false
+				}, {
+						"data" : {
+								"id" : "76",
+								"source" : "75",
+								"target" : "72",
+								"shared_name" : "South (interacts with) Kniephof",
+								"shared_interaction" : "interacts with",
+								"name" : "South (interacts with) Kniephof",
+								"interaction" : "interacts with",
+								"SUID" : 76,
+								"Weight" : 1,
+								"selected" : false
+						},
+						"selected" : false
+				}, {
+						"data" : {
+								"id" : "77",
+								"source" : "72",
+								"target" : "75",
+								"shared_name" : "Kniephof (interacts with) South",
+								"shared_interaction" : "interacts with",
+								"name" : "Kniephof (interacts with) South",
+								"interaction" : "interacts with",
+								"SUID" : 77,
+								"Weight" : 1,
+								"selected" : false
+						},
+						"selected" : false
+				}, {
+						"data" : {
+								"id" : "74",
+								"source" : "72",
+								"target" : "71",
+								"shared_name" : "Kniephof (interacts with) North",
+								"shared_interaction" : "interacts with",
+								"name" : "Kniephof (interacts with) North",
+								"interaction" : "interacts with",
+								"SUID" : 74,
+								"Weight" : 1,
+								"selected" : false
+						},
+						"selected" : false
+				}, {
+						"data" : {
+								"id" : "73",
+								"source" : "71",
+								"target" : "72",
+								"shared_name" : "North (interacts with) Kniephof",
+								"shared_interaction" : "interacts with",
+								"name" : "North (interacts with) Kniephof",
+								"interaction" : "interacts with",
+								"SUID" : 73,
+								"Weight" : 1,
+								"selected" : false
+						},
+						"selected" : false
+				} ]
+		}
+}
