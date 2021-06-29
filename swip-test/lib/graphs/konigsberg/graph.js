82f3e49b5168e7703321aa828ae085728ca09af4
twesleyb <twesleyb10@gmail.com>
2021-06-29 13:17:35 -0400

    done adding simple graph

--- swip-test/lib/graphs/konigsberg/graph.js
+++ swip-test/lib/graphs/konigsberg/graph.js
@@ -1,6 +1,20 @@
 let netw = cytoscape({
   container: document.getElementById('konigsberg-graph'),
   elements: data.elements,
+		style: [
+				{
+						selector: 'node',
+						style: {
+								'background-color': "rgb(58,127,182)", // node color
+								'border-color': "rgb(0,0,0)",
+								'border-width': 4,
+								'width': 75,
+								'height': 75,
+								'content': "data(name)",
+								'text-valign': "center"
+						}
+				}
+		],
   layout: {
     name: 'circle'
   }
