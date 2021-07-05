dae75b46e74a65ce9ac4a066d37f534a51fa37c7
twesleyb <twesleyb10@gmail.com>
2021-07-05 14:25:44 -0400

    update

--- SwipProteomics/README.md
+++ SwipProteomics/README.md
@@ -15,6 +15,12 @@ localhost
 gg=G
 ```
 
+### Vim Global find and replace
+```
+:%s/search_string/replacement_string/g
+```
+
 ### Appearance of cytoscape graphs
 * avoid apply global defaults like `width: '100%'`
 * style div element with css!
+* graphs/figures inserted with reveald3 only render when hosted
---
+++ SwipProteomics/lib/graphs/rogdi/code.js
@@ -0,0 +1,31 @@
+$.getJSON("data.json", function (data) {
+		//console.log(data);
+		var cy = cytoscape({
+				container: document.getElementById('cy'),
+				elements: data,
+				style: [
+						{
+								selector: 'node',
+								style: {
+										'label': 'data(Protein)',
+										'text-valign': 'center',
+										'width': 85,
+										'height': 85,
+										'background-color': 'data(Color)',
+										'border-color': 'black',
+										'border-width': 4,
+										'color': 'black',
+								}
+						}, {
+								selector: 'edge',
+								style: {
+										'width': '6px',
+										'color': 'red',
+								}
+						}
+				],
+				layout: {
+						name: 'cose'
+				}
+		});
+});
